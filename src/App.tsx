dca6b4c45c123a0c7348b6c05b83c14ad0e3e5dc
Tyrail Leverett <tyrailleverett@gmail.com>
2023-02-07 15:57:16 -0600

    feat: Added Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@mantine/core';
+
 
 function App() {
 
@@ -6,6 +8,9 @@ function App() {
 <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Button>
+      Click Me
+</Button>
     </div>
   )
 }
